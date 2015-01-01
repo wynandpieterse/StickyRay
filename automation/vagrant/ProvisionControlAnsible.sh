0699b2d65758b94888970d6a4b6b10cfbcf746fa
Wynand Pieterse <wpieterse.development@gmail.com>
2015-01-01 17:00:03 +0200

    Added option to automate installing Ansible with configuration file coming from system.

--- automation/vagrant/ProvisionControlAnsible.sh
+++ automation/vagrant/ProvisionControlAnsible.sh
@@ -25,7 +25,7 @@
 # Version 0.0.4
 #
 
-sudo apt-get install ansible -y
+sudo apt-get install ansible -y -o Dpkg::Options::="--force-confnew"
 
 ansible-galaxy install defunctzombie.coreos-bootstrap -p /vagrant/automation/roles
 ansible-playbook /vagrant/automation/Bootstrap.yml
