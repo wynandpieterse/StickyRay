87345c33f771cad4e798f16645cf6caacd6ad9b9
Wynand Pieterse <wpieterse.development@gmail.com>
2015-01-09 15:46:30 +0200

    Added synced folder to control VM.

--- automation/vagrant/DefineControlVM.rb
+++ automation/vagrant/DefineControlVM.rb
@@ -33,6 +33,9 @@ def defineControlVM(control, vmName)
 	control.vm.box = "https://cloud-images.ubuntu.com/vagrant/utopic/%s/utopic-server-cloudimg-amd64-vagrant-disk1.box" % $controlRequestImagePath
 	control.vm.network :private_network, ip: "10.10.10.10"
 
+	# Map the project directory to the VM
+	control.vm.synced_folder "../", "/application"
+
 	# Forward our Docker registry port to the outside world.
 	control.vm.network "forwarded_port", guest: 5000, host: $controlDockerRegistryPort, auto_correct: true
 
